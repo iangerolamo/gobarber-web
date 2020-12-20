aebcb90f5b349232a66099b5d1aec2cd14943086
Ian Gerolamo <iangerolamo@gmail.com>
2020-12-20 17:13:06 -0300

    Pagina de cadastro

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -9,9 +9,11 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
+
   & + div {
     margin-top: 8px;
   }
+
   input {
     flex: 1;
     background: transparent;
@@ -21,6 +23,7 @@ export const Container = styled.div`
       color: #666360;
     }
   }
+
   svg {
     margin-right: 16px;
   }

2d5b2b1d2e258b767f69238f0c6e2ed6a502f808
igerolamo <Kbbzkp53>
2021-02-04 10:40:56 -0300

    Adicionando e removendo Toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -25,23 +25,19 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { signIn } = useAuth();
   const { addToast } = useToast();
-
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({});
-
         const schema = Yup.object().shape({
           email: Yup.string()
             .email('Digite um e-mail válido')
             .required('E-mail obrigatório'),
           password: Yup.string().required('Senha obrigatória'),
         });
-
         await schema.validate(data, {
           abortEarly: false,
         });
-
         await signIn({
           email: data.email,
           password: data.password,
@@ -49,23 +45,24 @@ const SignIn: React.FC = () => {
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
-
           formRef.current?.setErrors(errors);
         }
 
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
-    }, [signIn, addToast],
+    },
+    [signIn, addToast],
   );
-
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
-
         <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
-
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
             name="password"
@@ -74,10 +71,8 @@ const SignIn: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Entrar</Button>
-
           <a href="forgot">Esqueci minha senha</a>
         </Form>
-
         <a href="signup">
           <FiLogIn />
           Criar conta
